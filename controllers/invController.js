5db1721870a825b74be1c9d6405f72979f63be4d
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-08-20 16:03:27 -0300

    "finished detail view"

--- controllers/invController.js
+++ controllers/invController.js
@@ -21,12 +21,12 @@ invCont.buildByInventory = async function (req, res, next) {
 invCont.buildInventoryByID = async function (req, res, next) {
     const inv_id = req.params.invId
     const data = await invModel.getInventoryByID(inv_id)
-    //const detailView = await utilities.buildDetailsView(data)
+    const detailView = await utilities.buildDetailsView(data)
     const invYear = data[0].inv_year
     const model = data[0].inv_model
     res.render("./inventory/detail", {
         title: invYear + " " + model,
-        //detailView,
+        detailView,
     })
 }
 
--- utilities/index.js
+++ utilities/index.js
@@ -2,7 +2,7 @@ const invModel = require("../models/inventoryModel")
 const Util = {}
 
 /* **************************************
-* Build the classification view HTML
+* Build the inventory view (cars)
 * ************************************ */
 Util.buildInventoryGrid = async function(data) {
     let grid
@@ -27,7 +27,7 @@ Util.buildInventoryGrid = async function(data) {
     grid += '<span class="fa fa-star checked"></span>'
     grid += '<span class="fa fa-star checked"></span>'
     grid += '</div>'
-    let x = Math.floor((Math.random() * 500) + 1);
+    let x = 500
     grid += '<span>'+vehicle.inv_rating+'</span>'
     grid += '</div>'
     grid += '<p class="text-truncate mb-4 mb-md-0">' + vehicle.inv_description + '</p>'
@@ -53,6 +53,52 @@ Util.buildInventoryGrid = async function(data) {
     grid = '<h3>Sorry, no matching vehicles could be found.</h3>'
     }
     return grid
+}
+
+
+/* **************************************
+* Build the details view
+* ************************************ */
+Util.buildDetailsView = async function (data){
+    let detailView
+    detailView = '<section class="py-5">'
+    if(data.length > 0){
+    data.forEach(vehicle => {
+    detailView += '<div class="container px-4 px-lg-5 my-5">'
+    detailView += '<div class="row gx-4 gx-lg-5 align-items-center">'
+    detailView += '<div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="'+ vehicle.inv_image +'" alt="'+ vehicle.inv_model +'"/></div>'
+    detailView += '<div class="col-md-6">'
+    detailView += '<div class="small mb-1">'+ vehicle.inv_year + ' - '+ vehicle.inv_color +'</div>'
+    detailView += '<h1 class="display-5 fw-bolder">'+vehicle.inv_model+'</h1>'
+    detailView += '<div class="fs-5 mb-5">'
+    let x = 500
+    let priceNumber = parseInt(vehicle.inv_price) + x
+    detailView += '<span class="text-decoration-line-through text-danger">'+new Intl.NumberFormat('en-US').format(priceNumber)+'</span>'
+    detailView += '<span class="text-success"> $'+ new Intl.NumberFormat('en-US').format(vehicle.inv_price) +'</span>'
+    detailView += '</div>'
+    detailView += '<p class="lead">'+vehicle.inv_description+'</p>'
+    detailView += '<div class="d-flex">'
+    detailView += '<button class="btn btn-outline-dark flex-shrink-0  me-2" type="button">'
+    detailView += '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">'
+    detailView += '<path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>'
+    detailView += '</svg> '
+    detailView += ' Add to cart'
+    detailView += '</button>'
+    detailView += '<button class="btn btn-dark" type="button">'
+    detailView += 'Quick buy now'
+    detailView += '</button>'
+    detailView += '</div>'
+    detailView += '</div>'
+    detailView += '</div>'
+    detailView += '</div>'
+
+    })
+    detailView += '</section>'
+} else {
+    detailView = '<h3>Sorry, no matching vehicle could be found.</h3>'
+}
+
+return detailView
 }
 
 
