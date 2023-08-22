5fa9b8b2ff25f3c91d67f0d0568aba2464e3576a
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-08-21 21:33:19 -0300

    "added handle error function and view"

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,11 +2,12 @@
 const express = require("express")
 const router = new express.Router()
 const invController = require("../controllers/invController")
+const utilities = require("../utilities/")
 
 // Route to build inventory
-router.get("/cars/", invController.buildByInventory);
+router.get("/cars/", utilities.handleErrors(invController.buildByInventory));
 
 // Route to build inventory by id
-router.get("/detail/:invId", (invController.buildInventoryByID))
+router.get("/detail/:invId", utilities.handleErrors(invController.buildInventoryByID))
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
 
 
 /* ***********************
@@ -26,12 +27,17 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", function(req, res) {
+app.get("/", utilities.handleErrors(function(req, res) {
 res.render("index", {title: "Home"})    
-})
+}))
 
 //Inventory Routes
 app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+    next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
 
 
@@ -41,6 +47,21 @@ app.use("/inv", inventoryRoute)
 
 const host = process.env.HOST
 const port = process.env.PORT
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+if(err.status == 404){message = err.message} else {message = 'Wrong way! Maybe try a different route?'}
+res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+})
+})
+
 
 /* ***********************
 * Log statement to confirm server operation
